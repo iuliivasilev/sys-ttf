cdbc7c07e2438d19c04ecfd1aafa2a853c6c86bb
vasiliev <yuliivasiliev@gmail.com>
2025-01-21 14:46:43 +0300

    Created scraper script (with zipping)

---
+++ Grafana/Scraper.py
@@ -0,0 +1,34 @@
+import constants as cnt
+import os
+import pandas as pd
+import requests
+import datetime
+import shutil
+
+from prometheus_api_client.utils import parse_datetime
+from prometheus_api_client import PrometheusConnect, MetricsList, MetricSnapshotDataFrame
+from tqdm import tqdm
+
+
+start_time = parse_datetime("60d")
+end_time = parse_datetime("now")
+chunk_size = datetime.timedelta(hours=1)
+parent_dir = os.path.join("D:", "SurvivalAnalysis", "TimeVarying", "CMCData")
+
+if __name__ == "__main__":
+    prom = PrometheusConnect(url=cnt.PROM_URL,
+                             headers={"Authorization": f"Bearer {cnt.TOKEN}"}, disable_ssl=False)
+    
+    dirname = os.path.join(parent_dir, str(end_time.date()))
+    os.makedirs(dirname, 0o774, exist_ok=True)
+    for metric in tqdm(prom.all_metrics(), ncols=80, ascii=True, desc='Total'):
+        metric_data = prom.get_metric_range_data(
+            metric,
+            start_time=start_time,
+            end_time=end_time,
+            chunk_size=chunk_size,
+        )
+        metric_df = MetricSnapshotDataFrame(metric_data)
+        metric_df.iloc[:, 1:].to_csv(os.path.join(dirname, f"{metric}.csv"))
+    shutil.make_archive(dirname, 'zip', dirname)
+    shutil.rmtree(dirname)
